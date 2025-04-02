f9a250d51f612e82d2f2ba77fbfd783eadb2c802
Hprimer <04200620z@gmail.com>
2025-04-02 21:44:24 +0400

    micro fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PackagesListComponent } from './packagesList/packagesList.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PackagesListComponent],
+  imports: [ PackagesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
